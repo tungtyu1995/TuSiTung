b070c89ee390c63b36a3a5af3b7ec2e0108e99c0
tungtyu1995 <tusytung@gmail.com>
2020-06-30 09:38:01 +0700

    sadsdf

--- src/introduction_to_java/bai_tap/QuyDoiTienTe.java
+++ src/introduction_to_java/bai_tap/QuyDoiTienTe.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class QuyDoiTienTe {
     public static void main(String[] args) {
         int usd;
-        int vnd = 23000;
+        int tiGia = 23000;
         int quyDoi;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập vào số tiền USD cần quy đổi");
         usd = scanner.nextInt();
-        quyDoi = usd * vnd;
+        quyDoi = usd * tiGia;
         System.out.println(usd + " USD " + " bằng " + quyDoi + " VND ");
     }
 }
