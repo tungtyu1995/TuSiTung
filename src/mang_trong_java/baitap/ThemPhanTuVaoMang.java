ed74161e873e47b20861c166ba4a0e1cbca43db3
tungtyu1995 <tusytung@gmail.com>
2020-07-01 11:10:10 +0700

    sadsdf

--- src/mang_trong_java/baitap/ThemPhanTuVaoMang.java
+++ src/mang_trong_java/baitap/ThemPhanTuVaoMang.java
@@ -7,14 +7,14 @@ public class ThemPhanTuVaoMang {
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhập số lượng phần tử mảng:");
         int size = sc.nextInt();
-        int[] array = new int[size+1];
+        int[] array = new int[size + 1];
         System.out.println("Nhập phần tử của mảng");
         for (int i = 0; i < size; i++) {
             System.out.print("Nhập phần tử thứ: " + (i + 1) + ": ");
             array[i] = sc.nextInt();
         }
         System.out.print("Mảng trước khi thêm vào:");
-        for (int val :array) {
+        for (int val : array) {
             System.out.print(val + " ");
         }
         System.out.print("Nhập phần tử muốn thêm vào mảng: ");
@@ -24,11 +24,11 @@ public class ThemPhanTuVaoMang {
 
         System.out.println("");
         System.out.print("Mảng sau khi thêm vào:");
-        for (int i = array.length - 1 ; i > index; i--) {
+        for (int i = array.length - 1; i > index; i--) {
             array[i] = array[i - 1];
         }
         array[index] = goal;
-        for (int val : array){
+        for (int val : array) {
             System.out.print(val + " ");
         }
     }
