b769f91701f7558327798fc69c90ce0169d66ab0
tungtyu1995 <tusytung@gmail.com>
2020-07-06 08:46:13 +0700

    sadsdf

---
+++ src/access_modifier/baitap/TestCircle.java
@@ -0,0 +1,9 @@
+package access_modifier.baitap;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle obj = new Circle();
+        System.out.println(obj.getRadius());
+        System.out.println(obj.getArea());
+    }
+}
