50c49f7ccadbbecc38768412cf3b6c949afdb97a
tungtyu1995 <tusytung@gmail.com>
2020-08-17 14:22:45 +0700

    sdfs

--- module3/2.ThietKeVaTaoCSDL/BaiTap/2.PhanTichThietKeCSDL.sql
+++ module3/2.ThietKeVaTaoCSDL/BaiTap/2.PhanTichThietKeCSDL.sql
@@ -2,91 +2,91 @@ drop database if exists carstore_database;
 create database carstore_database;
 use carstore_database;
 
-create table Offices(
-officeCode varchar(10) 	PRIMARY	KEY,
+create table offices(
+office_code varchar(10) 	PRIMARY	KEY,
 city varchar(50) not null,
 phone varchar(50) not null,
-addressLine1 varchar(50) not null,
-addressLine2 varchar(50),
+address_line1 varchar(50) not null,
+address_line2 varchar(50),
 state varchar(50) not null,
 country varchar(50) not null,
-postalCode varchar(15) not null
+postal_code varchar(15) not null
 );
-create table Employees(
-employeeNumber int 	PRIMARY	KEY,
-lastName varchar(50) not null,
-firstName varchar(50) not null,
+create table employees(
+employee_number int 	PRIMARY	KEY,
+last_name varchar(50) not null,
+first_name varchar(50) not null,
 email varchar(100) not null,
-jobTitle varchar(50) not null,
-Offices_officeCode varchar(10),
-FOREIGN	KEY	(Offices_officeCode) REFERENCES Offices(officeCode)
+job_title varchar(50) not null,
+offices_office_code varchar(10),
+FOREIGN	KEY	(offices_office_code) REFERENCES offices(office_code)
 
 );
-create table Customer(
-customer_Number int PRIMARY	KEY,
-customer_Name varchar(50) not null,
-contactLastName  varchar(50) not null,
-contactFirstName varchar(50) not null,
+create table customer(
+customer_number int PRIMARY	KEY,
+customer_name varchar(50) not null,
+contact_last_name  varchar(50) not null,
+contact_first_name varchar(50) not null,
 phone varchar(50) not null,
-address_Line1 varchar(50) not null,
-address_Line2 varchar(50),
+address_line1 varchar(50) not null,
+address_line2 varchar(50),
 city varchar(50) not null,
 stata varchar(50) not null,
-postal_Code varchar(15) not null,
+postal_code varchar(15) not null,
 country varchar(50) not null,
-credit_Limit double,
-Employee_employeeNumber int,
-FOREIGN	KEY	(Employee_employeeNumber) REFERENCES Employees(employeeNumber)
+credit_limit double,
+employee_employee_number int,
+FOREIGN	KEY	(employee_employee_number) REFERENCES employees(employee_number)
 
 );
-create table Orders(
-oderNumber int 	PRIMARY	KEY,
-orderDate date not null,
-requiredDate date not null,
-shippedDate date not null,
+create table orders(
+oder_number int 	PRIMARY	KEY,
+order_date date not null,
+required_date date not null,
+shipped_date date not null,
 `status` varchar(15),
 comnent text,
-quantityOedered int not null,
-priceEach double not null,
-Customer_customer_Number int,
-FOREIGN	KEY	(Customer_customer_Number) REFERENCES Customer(customer_Number)
+quantity_oedered int not null,
+price_each double not null,
+customer_customer_number int,
+FOREIGN	KEY	(customer_customer_number) REFERENCES customer(customer_number)
  );
 create table Paynemts(
-customer_Number int PRIMARY	KEY,
-checkNumber varchar(70) not null,
-paymentDate date not null,
+customer_number int PRIMARY	KEY,
+check_number varchar(70) not null,
+payment_date date not null,
 amount double not null,
-FOREIGN	KEY	(customer_Number) REFERENCES Customer(customer_Number)
+FOREIGN	KEY	(customer_number) REFERENCES customer(customer_number)
 
 );
-create table Productlines(
-productLine varchar(50) PRIMARY	KEY,
-textDescription text,
+create table product_lines(
+product_line varchar(50) PRIMARY	KEY,
+text_description text,
 image varchar(50)
 );
 create table Product(
-productCode varchar(15) PRIMARY	KEY,
-productNane varchar(50) not null,
-productScale varchar(10) not null,
-productVendor varchar(50) not null,
-productDescription text not null,
-quantityInStock int not null,
-buyPrice double not null,
-MSRP int not null,
-Productlines_productLine varchar(50),
-FOREIGN	KEY	(Productlines_productLine) REFERENCES Productlines(productLine)
+product_code varchar(15) PRIMARY	KEY,
+product_nane varchar(50) not null,
+product_scale varchar(10) not null,
+product_vendor varchar(50) not null,
+product_description text not null,
+quantity_inStock int not null,
+buy_price double not null,
+msrp int not null,
+Product_lines_product_line varchar(50),
+FOREIGN	KEY	(product_lines_product_line) REFERENCES product_lines(product_line)
 );
 
-create table Orders_has_Product(
-Orders_oderNumber int,
-FOREIGN	KEY	(Orders_oderNumber) REFERENCES Orders(oderNumber),
-Product_productCode varchar(15),
-FOREIGN	KEY	(Product_productCode) REFERENCES Product(productCode)
+create table orders_has_product(
+Orders_oder_number int,
+FOREIGN	KEY	(orders_oder_number) REFERENCES orders(oder_number),
+product_product_code varchar(15),
+FOREIGN	KEY	(product_product_code) REFERENCES product(product_code)
 );
-select*from Customer;
-select*from Orders;
-select*from Paynemts;
-select*from Product;
-select*from Productlines;
-select*from Employees;
-select*from Offices;
+select*from customer;
+select*from orders;
+select*from paynemts;
+select*from product;
+select*from product_lines;
+select*from employees;
+select*from offices;
